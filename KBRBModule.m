3ad53df30ddb324a5eeb60cdc567e22400f2f83f
Kevin Bradley <lechium@users.noreply.github.com>
2019-12-15 23:32:27 -0700

    Update KBRBModule.m

--- KBRBModule.m
+++ KBRBModule.m
@@ -2,32 +2,16 @@
 #import <Foundation/Foundation.h>
 #import <TVSystemMenuUI/TVSMButtonViewController.h>
 #include "KBRBModule.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <sys/types.h>
-#include <stdint.h>
-#include <spawn.h>
-#include <sys/wait.h>
 
 @interface PBSSystemService : NSObject
 +(id)sharedInstance;
--(void)deactivateApplication;
--(void)registerServiceProviderEndpoint:(id)arg1 forProviderType:(id)arg2 ;
--(void)endpointForProviderType:(id)arg1 withIdentifier:(id)arg2 responseBlock:(/*^block*/id)arg3 ;
--(void)launchKioskApp;
--(void)sleepSystemForReason:(id)arg1 ;
--(void)wakeSystemForReason:(id)arg1 ;
 -(void)relaunchBackboardd;
 @end
 
 @interface PBSSystemServiceConnection : NSObject
 +(id)sharedConnection;
 -(id)systemServiceProxy;
--(BOOL)isValid;
--(void)setValid:(BOOL)arg1;
 @end
-
 
 @implementation KBRBModule
 
@@ -35,19 +19,16 @@
     return 2;
 }
 
--(id)contentViewController {
-    
+-(id)contentViewController {    
     TVSMButtonViewController *buttonController = (TVSMButtonViewController*)[super contentViewController];
     [buttonController setTitleText:@"Restart"];
     [buttonController setSecondaryText:@"backboardd"];
     NSString *packageFile = [[self bundle] pathForResource:@"RespringIcon" ofType:@"png"];
     [buttonController setImage:[[UIImage imageWithContentsOfFile:packageFile] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
-
     return buttonController;
 }
 
 -(void)handleAction {
-
     id connection = [PBSSystemServiceConnection sharedConnection];
     id ssp = [connection systemServiceProxy];
     [ssp relaunchBackboardd];
